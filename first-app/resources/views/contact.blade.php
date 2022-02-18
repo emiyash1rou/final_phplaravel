01a2b264d0389fcff424b78f350ba3f5a3beba9e
emiyash1rou <mumbukan.work@gmail.com>
2022-02-18 19:24:49 +0800

    midpoint of tutorial

--- first-app/resources/views/contact.blade.php
+++ first-app/resources/views/contact.blade.php
@@ -1,1 +1,22 @@
-<h1> This is contact number </h1>
+
+        
+       @extends('header')
+       @section('title',"Contact Us")
+       @section('content')
+
+
+            <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
+                <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
+                  <h1> Contact Us</h1>
+                </div>
+<p> in here</p>
+                
+            </div>
+            @endsection
+            @section('scripts')
+            <script>
+alert("contact us")
+            </script>
+            
+            @endsection
+     
--- first-app/routes/web.php
+++ first-app/routes/web.php
@@ -13,7 +13,7 @@ use App\Http\Controllers\HomeController;
 |
 */
 Route::get('/',[HomeController::class,'index']);
-Route::get('/about',[HomeController::class,'about']);
+Route::get('/about/about',[HomeController::class,'about']);
 Route::get('/contact',[HomeController::class,'contact']);
 
 
